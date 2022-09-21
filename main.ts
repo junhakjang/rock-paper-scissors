cb72b68e6a633ee627cab5548bf0ce728eb4d0fd
junhakjang <100299253+junhakjang@users.noreply.github.com>
2022-09-20 20:23:33 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,55 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == Rock_Index) {
+        if (My_hand == Paper_Index) {
+            basic.showString("W")
+            radio.sendString("L")
+            Score += 1
+        } else if (My_hand == Rock_Index) {
+            basic.showString("T")
+            radio.sendString("T")
+        } else {
+            basic.showString("L")
+            radio.sendString("W")
+        }
+    }
+    if (receivedNumber == Paper_Index) {
+        if (My_hand == Scissor_index) {
+            basic.showString("W")
+            radio.sendString("L")
+            Score += 1
+        } else if (My_hand == Paper_Index) {
+            basic.showString("T")
+            radio.sendString("T")
+        } else {
+            basic.showString("L")
+            radio.sendString("W")
+        }
+    }
+    if (receivedNumber == Scissor_index) {
+        if (My_hand == Rock_Index) {
+            basic.showString("W")
+            radio.sendString("L")
+            Score += 1
+        } else if (My_hand == Scissor_index) {
+            basic.showString("T")
+            radio.sendString("T")
+        } else {
+            basic.showString("L")
+            radio.sendString("W")
+        }
+    }
+})
 input.onButtonPressed(Button.A, function () {
     My_hand += -1
     Choose_shape()
     if (My_hand < 1) {
-        basic.showString("Too low")
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
     }
 })
 function Choose_shape () {
@@ -16,18 +63,21 @@ function Choose_shape () {
         Paper_Image.showImage(0)
     }
 }
-radio.onReceivedString(function (receivedString) {
-	
-})
 input.onButtonPressed(Button.B, function () {
     My_hand += 1
     Choose_shape()
     if (My_hand > 3) {
-        basic.showString("Too High ")
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
     }
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    radio.sendNumber(My_hand)
 })
 let My_hand = 0
 let Scissor_Image: Image = null
@@ -39,6 +89,7 @@ let Scissor_index = 0
 Scissor_index = 1
 Paper_Index = 2
 Rock_Index = 3
+let Score = 0
 Paper_Image = images.createImage(`
     # # # # #
     # # # # #
@@ -61,5 +112,10 @@ Scissor_Image = images.createImage(`
     # # . . #
     `)
 basic.forever(function () {
-    radio.sendNumber(My_hand)
+    if (Score >= 2) {
+        basic.clearScreen()
+        music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
+        basic.showString("You Win")
+    }
+    basic.clearScreen()
 })
